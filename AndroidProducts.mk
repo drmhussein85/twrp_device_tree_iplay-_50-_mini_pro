dc6141de6f7890ade5a2439014260fd70322f76b
drmhussein85 <66475390+drmhussein85@users.noreply.github.com>
2024-01-12 21:56:46 +0300

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_iPlay_50_mini_Pro.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_iPlay_50_mini_Pro-user \
+    twrp_iPlay_50_mini_Pro-userdebug \
+    twrp_iPlay_50_mini_Pro-eng
